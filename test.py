0c500ff19f0858c42c04dcdec77605d73571ae66
Praise James <66879837+DrPraze@users.noreply.github.com>
2023-07-04 00:50:41 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
-import requests
+import socket
 
-BASE = "http://127.0.0.1:5000/search"
-response = requests.post(BASE, json={"query":"https://slakenet.com.ng/","room":"emailroom"});
-print(response.json())
+def main():
+    host = '127.0.0.1'
+    port = 12345
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+    print("[*] Connected to server")
+
+    while True:
+        message = input("Enter a message (type 'exit' to quit): ")
+        if message == 'exit':
+            break
+
+        # Send the message to the server
+        client_socket.send(message.encode('utf-8'))
+
+        # Receive and print the processed result from the server
+        result = client_socket.recv(1024).decode('utf-8')
+        print(f"Server says: {result}")
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
+  
